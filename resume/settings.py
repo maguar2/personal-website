2c027c43f2475c194bcb516dc595bff7a374cfaf
Patrick Idungafa <patrickidungafa2@gmail.com>
2021-08-20 01:09:20 +0100

    I editted some deprecated codes in settings, reworked my Secret key

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+with open('main_resume/others/s_k.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -43,8 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    #Custom middleware
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    #Django Middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -85,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'resume',
         'USER': 'postgres',
-        'PASSWORD': os.environ['PASSWORD'],
+        'PASSWORD': 'Ilyadpat123',
         'HOST':'Localhost',
         'PORT':'8001',
     }
@@ -130,15 +132,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
 
 if not DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
