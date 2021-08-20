1f56dfdecf8cbc3d9fa7f78b16b7603aebdbc5d0
Patrick Idungafa <patrickidungafa2@gmail.com>
2021-08-20 20:30:41 +0100

    removed a wrong staticfile code

--- resume/settings.py
+++ resume/settings.py
@@ -163,4 +163,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
